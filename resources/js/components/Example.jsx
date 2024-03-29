fc275ff80d3b964e7fbaf370d2ae7f9b52d2ae0f
folza <fodorzoltan317@gmail.com>
2024-03-29 15:17:55 +0100

    Header 1

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './components/Navbar';
 
 function Example() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example ComponentAAWAWAaa</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <Navbar/>
+        </>
     );
 }
 
---
+++ resources/js/components/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <p className="w-25 font-24 font-bold bg-white p-5 m-15 border-bottom border-right border-radius-10">FOLZA</p>
+                <p className="w-50 bg-white p-5 m-15 border-bottom border-right border-radius-10">HTML kurzus kezdőknek!</p>
+            </div>
+        </>
+    );
+}
