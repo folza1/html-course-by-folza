0f7a1f9305918ba9923b2179bad3c8771f307d86
folza1 <106752284+folza1@users.noreply.github.com>
2024-03-31 21:34:20 +0200

    VS Code

--- resources/js/components/components/sidebar/Sidebar.jsx
+++ resources/js/components/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import './sidebar.css';
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar content</h1>
+            <h1>Sidebar content My</h1>
             <h1>Sidebar content</h1>
             <h1>Sidebar content</h1>
             <h1>Sidebar content</h1>
