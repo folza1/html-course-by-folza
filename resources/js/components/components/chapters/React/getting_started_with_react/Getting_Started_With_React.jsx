1278af8933de2d7d8116609259631cf04cc6908a
folza <fodorzoltan317@gmail.com>
2024-04-03 13:10:25 +0200

    Before MX

---
+++ resources/js/components/components/chapters/React/getting_started_with_react/Getting_Started_With_React.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './getting_strarted_with_react.css';
+
+export default function Getting_Started_With_React() {
+    return (
+        <div>
+            <p>
+                Ebben a cikkben hello-t mondunk a React-nak. Egy kicsit részletesebben fel fogjuk fedezni
+                a React hátterét és hogy milyen esetekben használjuk, beállítunk egy olyan eszközláncot
+                a helyi számítógépünkön, amelyet egy React alkamazás során szoktak használni aztán készítünk
+                és kipróbálunk egy egyszerű kezdő alkalmazást - szóval tanulunk egy kicsit a React működéséről.
+            </p>
+
+            <div>
+            <p>
+                Előfeltételek: a HTML, CSS és JavaScript lényegét ismernie kell és legyenek ismeretek
+                a terminal/parancssor használatáról
+            </p>
+
+                <p>
+                A React egy HTML a JavaScript-ban szintaxist használ, amit JSX-nek neveznek
+                (JavaScript és XML).
+                Ha ismered a HTML és a Javascript lényegét az segíteni fog a JSX megtanulásában és segít majd
+                azonosítani a hibákat, hogy azok a Javascript-hez vagy a React-hez
+                kapcsolódnak-e?.
+             </p>
+            </div>
+        </div>
+    );
+}
--- resources/js/components/components/feed/Feed.jsx
+++ resources/js/components/components/feed/Feed.jsx
@@ -4,13 +4,7 @@ import './feed.css';
 export default function Feed() {
     return (
         <div className="feed">
-            <h1>Feed content</h1>
-            <h1>Feed content</h1>
-            <h1>Feed content</h1>
-            <h1>Feed content</h1>
-            <h1>Feed content</h1>
-            <h1>Feed content</h1>
-            <h1>Feed content</h1>
+
         </div>
     );
 }
