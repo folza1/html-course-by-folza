e89c94430901d0b99d1795fd355661014d01bc15
folza <fodorzoltan317@gmail.com>
2024-04-08 22:25:10 +0200

    Tools and testing 7 Safety Net

--- resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Client-Side-Web-Development-Tools/Client-Side-Tooling-Overview.jsx
+++ resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Client-Side-Web-Development-Tools/Client-Side-Tooling-Overview.jsx
@@ -17,7 +17,11 @@ export default function Client_Side_Tooling_Overview() {
             <Article>
                 <Paragraph>
                     <div className="bold inline">Előfeltételek:</div>
-                    Alapismeretek a HTML, CSS, és Javascript nyelvekkel.
+                    Alapismeretek a <a target="_blank"
+                                       href="https://developer.mozilla.org/en-US/docs/Learn/HTML">HTML</a>, <a
+                    target="_blank" href="https://developer.mozilla.org/en-US/docs/Learn/CSS">CSS</a> és a <a
+                    target="_blank"
+                    href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript">Javascript</a> nyelvekkel.
                 </Paragraph>
                 <Paragraph>
                     <div className="bold inline">Cél:</div>
@@ -56,6 +60,38 @@ export default function Client_Side_Tooling_Overview() {
                     Ebben a cikkben, nem válaszolunk meg minden kérdést a web eszközkezelésről, de adunk egy hasznos
                     kezdő pontot az alapok megértéséhez, amelyre építhetsz. Mint bármelyik komplex téma, jó ha kicsiben
                     kezded és fokozatosan fejleszted magad előrehaladottabb használat felé.
+                </Paragraph>
+            </Article>
+            <Article>
+                <h1>A modern eszköz ökoszisztéma</h1>
+                <Paragraph>
+                    A mai fejlesztői eszköz ökoszisztéma hetalmas, szóval hasznos, ha van egy szélesebb rálátásunk
+                    arról milyen főbb problémákat oldanak meg. Ha beírod a kedvenc keresődbe a "front-end fejlesztői
+                    eszközök"-et hatalmas számú találatot fogsz látni, szövegszerkesztőtől kezdve, a böngészőkig, az
+                    olyan fajta tollig amivel jegyzeteket készíthetsz.
+                </Paragraph>
+                <Paragraph>
+                    Bár a szövegszerkesztőd választása egy eszköz választás, ez a cikk tovább is megy, fókuszálva olyan
+                    eszközökre amelyek segítenek még hatékonyabb kód szerkesztésére.
+                </Paragraph>
+                <Paragraph>
+                    Magasabb szintről nézve, ebbe a három kategóriába teheted a kliens oldali eszközöket az általuk
+                    megoldott problémák szerint:
+                    <li>Biztonsági háló - Eszközök melyek hasznosak a kód fejlesztés során</li>
+                    <li>Transzformáció - Eszközök amelyek átalakítják a kódot valamilyen módon, pl. átfordítani egy
+                        középszintű nyelvet Javascript-té amit a böngésző megért
+                    </li>
+                    <li>Fejlesztés utáni - Eszközök melyek hasznosak a megírt kód után, mit a tesztelés és a telepítő
+                        eszközök
+                    </li>
+                </Paragraph>
+                <Paragraph>
+                    Most nézzük meg egyesével őket egy kicsit részletesebben:
+                </Paragraph>
+
+                <h2>Biztonsági háló></h2>
+                <Paragraph>
+                    Ezek az eszközök amelyekkel a kód írása egy kicsit jobb.
                 </Paragraph>
             </Article>
         </div>
