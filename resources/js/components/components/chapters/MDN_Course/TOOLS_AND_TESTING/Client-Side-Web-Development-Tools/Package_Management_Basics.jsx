891bf78c59affc3d2ffbec702e6c3caa59cb4929
folza1 <fodorzoltan317@gmail.com>
2024-04-24 15:22:03 +0200

    Package Management Basics 3 Parcel

--- resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Client-Side-Web-Development-Tools/Package_Management_Basics.jsx
+++ resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Client-Side-Web-Development-Tools/Package_Management_Basics.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Paragraph from "../../REACT/paragraph/Paragraph.jsx";
 import Article from "../../REACT/article/Article.jsx";
 import BgLightgray from "../../REACT/bg-lightgray/BgLightgray.jsx";
+import Note from "../../REACT/note/Note.jsx";
 
 export default function Package_Management_Basics() {
     return (
@@ -97,7 +98,9 @@ export default function Package_Management_Basics() {
                     <li>Meg kell találni a helyes csomag Javascript fájlokat.</li>
                     <li>Ellenőrizni kell őket, hogy biztosan nem tartalmaznak-e bármilyen ismert sebezhetőséget.</li>
                     <li>Le kell tölteni őket és a megfelelő helyre kell rakni őket a projektben.</li>
-                    <li>A kódban include-olni kell a csomagokat (ehhez Javascript modulokat kell használni, egy másik
+                    <li>A kódban include-olni kell a csomagokat (ehhez <a
+                        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules" target="_blank">Javascript
+                        modulokat</a> kell használni, egy másik
                         téma amit érdemes olvasni és megérteni).
                     </li>
                     <li>Ugyanezeket kell tenni minden csomag alfüggőségével amely lehet több tíz vagy több száz.</li>
@@ -114,6 +117,74 @@ export default function Package_Management_Basics() {
                     telepíteni a függőségeidet. Amint már említettük az előző cikkben, a függőségek telepíthetők
                     globálisan és lokálisan a projektbe. Habár több előnye van a globális telepítésnek, az előnye a
                     lokális telepítésnek sokkal fontosabb (például a hordozhatóság és a verzió zárolás).
+                </Paragraph>
+
+                <Paragraph>
+                    Például, ha a projekted bizonyos Webpack beállításokra támaszkodik, akkor biztos akarsz benne lenni,
+                    hogy, ha egy másik gépre akarod telepíteni a projektedet vagy vissza akarsz térni rá sok idő
+                    elteltével, még akkor is működjön a beállításokkal. Ha egy másik Webpack van installálva lehet nem
+                    kompatibilis. Hogy elkerüljük ezt a függőségeket lokálisan telepítjük a projekthez.
+                </Paragraph>
+
+                <Paragraph>
+                    Hogy lásd a helyi függőségeket igazán világosan, annyit kell tenned hogy letöltesz és futtatsz egy
+                    létező projektet. Ha működik az összes függőséggel együtt, akkor helyi függőségei vannak a code
+                    hordozhatóságnak köszönhetően.
+                </Paragraph>
+
+                <Note>
+                    <Paragraph>
+                        <div className="bold inline">Megjegyzés:</div>
+                        Az npm nem az egyetlen elérhető csomagkezelő. Egy másik népszerű alternatív csomagkezelő a <a
+                        href="https://yarnpkg.com/" target="_blank">Yarn</a
+                    >.
+                        A Yarn különböző algoritmus alapján oldja meg a függőségeket amely gyorsabb felhasználói
+                        élményhez vezet. Van egy másik kiemelkedő kliens is mint a <a href="https://pnpm.io/"
+                                                                                      target="_blank">pnpm</a>.
+                    </Paragraph>
+                </Note>
+            </Article>
+
+            <Article>
+
+                <h1>Csomag jegyzékek</h1>
+
+                <Paragraph>
+                    Ahhoz hogy egy csomagkezelő működjön tudni kell hogy honnan lehet a csomagokat telepíteni és
+                    ilyenkor jönnek szóba a csomag jegyzékek másnéven csomag regiszterek. A regiszter egy központi hely
+                    ahol a csomag publikálva van és ahonnan telepíthető. Az npm egy csomagkezelő és ugyan ez a neve a
+                    gyakran használt Javascript csomag regiszternek is. Az npm regiszter az <a
+                    href="https://www.npmjs.com/" target="_blank">npmjs.com</a>-on elérhető.
+                </Paragraph>
+
+                <Paragraph>
+                    Az npm nem az egyetlen lehetőség. Kezelheted a saját csomag regiszteredet is. Termékek mint például
+                    a <a href="https://azure.microsoft.com/en-gb/" target="_blank">Microsoft Azure</a> lehetővé teszi,
+                    hogy készíts npm regiszter helyettesítőket amivel felülírhatsz
+                    vagy lezárhatsz csomagokat, vagy a <a href="https://github.com/features/packages" target="_blank">GitHub
+                    is biztosít csomag regiszter szolgáltatásokat</a> és egyre
+                    több lehetőség lesz, ahogyan az idő múlik.
+                </Paragraph>
+
+                <Paragraph>
+                    Ami fontos, hogy a számodra legmegfelelőbb regisztert válaszd ki. Sok projekt használ npm-et és mi
+                    is ennél maradunk a példáink során ebben a modulban.
+                </Paragraph>
+            </Article>
+
+            <Article>
+                <h1>A csomag ökoszisztéma használata</h1>
+
+                <Paragraph>
+                    Most fussunk át egy példán, hogy elkezdjünk egy csomagkezelőt és regisztert használni. Telepítsünk
+                    egy
+                    parancssoros segédet hozzá.
+                </Paragraph>
+
+                <Paragraph>
+                    A <a href="https://parceljs.org/" target="_blank">Parcel</a> egy másik eszköz amit a fejlesztők gyakran használnak a fejlesztés során. A Parcel abban
+                    okos hogy meg tudja nézni a kódunk tartalmát, hogy meghívja a függőségeket és automatikusan telepít
+                    bármilyen függőséget amire a kódunknak szüksége van. Plusz, automatikusan build-eli is a kódunkat.
                 </Paragraph>
             </Article>
         </div>
