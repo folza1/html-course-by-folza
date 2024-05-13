3b6a63af203b6c0d80f2e57118910a48a5496437
folza1 <fodorzoltan317@gmail.com>
2024-05-13 15:20:00 +0200

    Introducing_A_Complete_Toolchain mini_site pic

--- resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Client-Side-Web-Development-Tools/Introducing_A_Complete_Toolchain.jsx
+++ resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Client-Side-Web-Development-Tools/Introducing_A_Complete_Toolchain.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Paragraph from "../../REACT/paragraph/Paragraph.jsx";
 import Article from "../../REACT/article/Article.jsx";
+import Note from "../../REACT/note/Note.jsx";
+import mini_site from "./img/4.mini_site.png";
+
 
 export default function Introducing_A_Complete_Toolchain() {
     return (
@@ -33,9 +36,29 @@ export default function Introducing_A_Complete_Toolchain() {
 
             <Paragraph>
                 Tényleg végtelen számú kombináció van az eszközökből és a módjukból ahogyan használhatjuk őket, de amit
-                ebben a cikkben és a következőben fogsz látni egy módja annak hogy működő eszközöket használj fel a
+                ebben a cikkben és a következőben fogsz látni az egy módja annak hogy működő eszközöket használj fel a
                 projektedhez.
             </Paragraph>
+
+            <Note>
+                <Paragraph>
+                    <div className="bold inline mr-1">Megjegyzés:</div>
+                    Érdemes azt is megjegyezni, hogy nem mindegyik eszköznek kell a parancssorban futnia ezek közül. Sok
+                    mai kódszerkesztőnek mint a VS Code-nak vannak beépített támogató eszközei plugin-okon keresztül.
+                </Paragraph>
+            </Note>
+
+            <Article>
+                <h1>Az esettanulmányunk bemutatása</h1>
+
+                <Paragraph>
+                    Az eszközlánc amelyet elkészítünk ebben a cikkben egy mini oldalt fogunk elkészíteni amely adatot
+                    listáz (egy NASA API-tól kapja az adatot) olyan potenciálisan veszélyes űrobjektumokról amelyek
+                    veszélyeztetik az életünket itt a Föld-ön. Így néz ki az oldal:
+                </Paragraph>
+
+                <div className="image-container"><img src={mini_site} alt="Mini Site"/></div>
+            </Article>
         </div>
     )
 };
