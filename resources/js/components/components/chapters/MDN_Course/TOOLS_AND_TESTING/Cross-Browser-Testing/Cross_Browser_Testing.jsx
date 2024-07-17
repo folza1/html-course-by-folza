f180eae35d3d673ade4dd58b488c81e0569f8a04
folza1 <fodorzoltan317@gmail.com>
2024-07-17 20:38:54 +0200

    Test 2. article

--- resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Cross-Browser-Testing/Cross_Browser_Testing.jsx
+++ resources/js/components/components/chapters/MDN_Course/TOOLS_AND_TESTING/Cross-Browser-Testing/Cross_Browser_Testing.jsx
@@ -22,8 +22,8 @@ export default function Cross_Browser_Testing() {
                 és hogyan használj automatizációt, hogy felgyorsítsd a
                 tesztelést.
             </Paragraph>
+            <h1>Előfeltételek</h1>
             <Paragraph>
-                <div className="bold inline mr-1">Előfeltételek:</div>
                 Igazán tudnod kellene a{" "}
                 <a
                     href="https://developer.mozilla.org/en-US/docs/Learn/HTML"
@@ -48,9 +48,47 @@ export default function Cross_Browser_Testing() {
                 nyelvek alapjait először, mielőtt elkezded használni az itt
                 részletezett eszközöket.
             </Paragraph>
-            You should really learn the basics of the core HTML, CSS, and
-            JavaScript languages first before attempting to use the tools
-            detailed here.
+            <Article>
+                <h1>Útmutatók</h1>
+                <a
+                    href="https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Introduction"
+                    target="_blank"
+                >
+                    Bevezetés a böngészők közötti tesztelésbe
+                </a>
+                <Paragraph>
+                    Ezzel a cikkel kezdjük a modult, amely egy áttekintést ad a
+                    böngészők közötti tesztelés témában, megválaszolva a kérdést
+                    mi is a böngészők közötti tesztelés, mik a leggyakoribb
+                    típusú problémák, amelyek felmerülnek és mi a fő
+                    megközelítés a teszteléshez, a problémák azonosításához és
+                    megoldásához.
+                </Paragraph>
+                <a
+                    href="https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Testing_strategies"
+                    target="_blank"
+                >
+                    Stratégiák a tesztelés kivitelezéséhez
+                </a>
+                <Paragraph>
+                    Aztán, belemegyünk a tesztelés kivitelezésébe, azonosítjuk a
+                    célközönséget (milyen böngészők, eszközök és más részeket
+                    kell majd tesztelned), lo-fi (low fidelity) tesztelési
+                    stratégiákat nézünk meg (amely során kapsz majd egy sor
+                    eszköz típust és néhány virtuális gépet amikkel alkalmi
+                    tesztelést hajthatsz végre ha szükséges), magasabb szintű
+                    technikai stratégiákat ismerünk meg (automatizáció
+                    használata és erre a célra készült tesztelő alkalmazások
+                    használata) és a tesztelést felhasználói csoportokkal.
+                </Paragraph>
+            </Article>
+            Next, we drill down into carrying out testing, looking at
+            identifying a target audience (e.g. what browsers, devices, and
+            other segments should you make sure are tested), lo-fi testing
+            strategies (get yourself a range of devices and some virtual
+            machines and do ad hoc tests when needed), higher tech strategies
+            (automation, using dedicated testing apps), and testing with user
+            groups.
         </div>
     );
 }
